69c386cb282db4d1761a418066f5146ecbbacf30
davidkahh. <lndavid09@gmail.com>
2026-09-23 08:08:13 +0200

    Add files via upload

--- login.sql
+++ login.sql
@@ -1,21 +1,74 @@
-CREATE DATABASE IF NOT EXISTS parkside_db;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2025. Sze 08. 15:00
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `felhasznalok`
+--
+CREATE DATABASE parkside_db;
 USE parkside_db;
 
-CREATE TABLE IF NOT EXISTS `felhasznalok` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100) NOT NULL,
-  `username` VARCHAR(100) NOT NULL UNIQUE,
-  `email` VARCHAR(150) NOT NULL UNIQUE,
-  `password` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`)
+
+CREATE TABLE `felhasznalok` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
-INSERT INTO `felhasznalok` (`name`, `username`, `email`, `password`)
-VALUES
-  ('Kovacs Janos', 'Kovi', 'kovi@example.com', '$2y$10$krazRmqJQ8ZYwg.9xaKm3ereQQfXx1cO5fbqm6AiwY8bw3hm2jdqq'),
-  ('Nagy Erika', 'lopeaonm', 'erika@example.com', '$2y$10$10wGTVCJCD3nX4qoSD8yZ.v3tzhdLQvhmH3wpfR0qJoUTNsLhwzwW'),
-  ('Szabo Peter', 'petya', 'peter@example.com', '$2y$10$2Rx2Qekg5n2KH.Z.BgmNauJ/w/DKhlPbovZJec.DstpamJ746UXBK')
-ON DUPLICATE KEY UPDATE
-  `name` = VALUES(`name`),
-  `password` = VALUES(`password`);
+--
+-- A tábla adatainak kiíratása `felhasznalok`
+--
 
+INSERT INTO `felhasznalok` (`id`, `name`, `password`, `username`) VALUES
+(1, 'Kovacs Janos', 'jelszo123', 'Kovi'),
+(2, 'Nagy Erika', 'titok456' ,'lopeaonm'),
+(3, 'Szabo Peter', 'pass789' , 'petya'),
+(4, 'Toth Anna', 'alma2025', 'anna'),
+(5, 'Kiss Bela', 'secure!pass', 'bela');
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
